48aae96a2ee2c0efcd1fc3e60e36057a1fa68af2
zhuangjie <2119299531@qq.com>
2022-12-30 20:48:22 +0800

    'deploy'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,8 @@
-
+const moment = require('moment');
 module.exports = {
   title: "肥小猪",
   description: "肥小猪的笔记",
+  base: "/docs/",
   themeConfig: {
 	lastUpdated: "更新时间",
     logo: '/imgs/logo.png',
@@ -21,6 +22,18 @@ module.exports = {
 	['meta', { name: 'keywords', content: '小庄的博客 小庄的笔记 zhuangjie 庄杰' }]
 	
   ],
-  plugins: []
+  plugins: [
+	[
+	  '@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+          // 不要忘了安装 moment
+          const moment = require('moment')
+          moment.locale("zh-cn")
+          return moment(timestamp).format("LLLL")
+        }
+      }
+	]
+  ]
   
 }
